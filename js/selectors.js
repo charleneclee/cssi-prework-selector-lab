e12beb1d6fb326d222b78f184bba3c3134cdd02d
charleneclee <cclee@google.com>
2017-05-18 15:31:53 -0700

    finished selectors lab

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,26 @@
 'use-strict';
 
+function h1Selector() {
+  return $('h1');
+}
+
+function liIn01Selector() {
+  return $('ol li');
+}
+
+
+function linkSelector() {
+  return $('a #my-id .myclass');
+}
+
+function catSelector() {
+  return $('img.alt ="cat sleeping"');
+}
+
+function checkboxInputSelector() {
+  return $(':checkbox');
+}
+
 /* you can return a selector by using the synax: return $('selector').
 function pSelector(){
   return $('p');
